b8b68dbdfc7d2ce8cd3951dd550d1212f8c8b44e
zacharykratochvil <zacharykratochvil@gmail.com>
2022-08-30 07:54:28 -0400

    fixed bug and added keyword search feature

--- scripts/word_detector.py
+++ scripts/word_detector.py
@@ -10,7 +10,24 @@ from actionlib_msgs.msg import GoalStatus, GoalStatusArray
 from respeaker_ros.msg import StampedAudio
 from sound_play.msg import SoundRequest, SoundRequestAction, SoundRequestGoal
 from speech_recognition_msgs.msg import SpeechRecognitionCandidates
+from pocketsphinx.pocketsphinx import Decoder
 
+keywords=[
+("stand up", .95),
+("stand", 1.1),
+("up", 1.18),
+
+("sit down", 1.0),
+("sit up", .99),
+("sit", 1.05),
+
+("lie down", 1.1),
+("down", 1.2),
+
+("k dog", .88),
+("hello", 1.1),
+("hi there", 1.0)
+]
 
 class SpeechToText(object):
     def __init__(self):
@@ -20,53 +37,14 @@ class SpeechToText(object):
         # language of STT service
         self.language = rospy.get_param("~language", "en-US")
         # ignore voice input while the robot is speaking
-        self.self_cancellation = rospy.get_param("~self_cancellation", True)
-        # time to assume as SPEAKING after tts service is finished
-        self.tts_tolerance = rospy.Duration.from_sec(
-            rospy.get_param("~tts_tolerance", 1.0))
-
-        self.recognizer = SR.Recognizer()
-
-        self.tts_action = None
-        self.last_tts = None
-        self.is_canceling = False
-        if self.self_cancellation:
-            self.tts_action = actionlib.SimpleActionClient(
-                "sound_play", SoundRequestAction)
-            if self.tts_action.wait_for_server(rospy.Duration(5.0)):
-                self.tts_timer = rospy.Timer(rospy.Duration(0.1), self.tts_timer_cb)
-            else:
-                rospy.logerr("action '%s' is not initialized." % rospy.remap_name("sound_play"))
-                self.tts_action = None
-
         self.pub_speech = rospy.Publisher(
             "speech_to_text", SpeechRecognitionCandidates, queue_size=2)
         self.sub_audio = rospy.Subscriber("speech_audio", StampedAudio, self.audio_cb, queue_size=2)
 
-    def tts_timer_cb(self, event):
-        stamp = event.current_real
-        active = False
-        for st in self.tts_action.action_client.last_status_msg.status_list:
-            if st.status == GoalStatus.ACTIVE:
-                active = True
-                break
-        if active:
-            if not self.is_canceling:
-                rospy.logdebug("START CANCELLATION")
-                self.is_canceling = True
-                self.last_tts = None
-        elif self.is_canceling:
-            if self.last_tts is None:
-                self.last_tts = stamp
-            if stamp - self.last_tts > self.tts_tolerance:
-                rospy.logdebug("END CANCELLATION")
-                self.is_canceling = Falser
+        self.recognizer = SR.Recognizer()
 
     def audio_cb(self, msg):
 
-        if self.is_canceling:
-            rospy.loginfo("Speech is cancelled")
-            return
         if rospy.get_rostime().secs - msg.stamp.secs > 2:
             rospy.loginfo("Old speech discarded")
             return
@@ -78,7 +56,7 @@ class SpeechToText(object):
         try:
             rospy.loginfo("Waiting for result %d" % len(data.get_raw_data()))
             result = self.recognizer.recognize_sphinx(
-                data, language=self.language, show_all=False) #, key=None
+                data, language=self.language, show_all=False, keyword_entries=keywords)
             rospy.loginfo(result)
 
             msg = SpeechRecognitionCandidates(transcript=result)
